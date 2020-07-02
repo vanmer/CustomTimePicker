95b0b28a9da2bba130d8a49e0dedd4960e791114
vanmer <60948275+vanmer@users.noreply.github.com>
2020-07-02 16:13:04 +0200

    showing actual time, hour and minute change functions

--- main.js
+++ main.js
@@ -1,24 +1,61 @@
 // select the elements
-const time_picker_element = document.querySelector(".time-picker");
-const hr_element = document.querySelector(".time-picker .hour .hr");
-const min_element = document.querySelector(".time-picker .minute .min");
+const time_picker_element = document.querySelector('.time-picker');
+const hr_element = document.querySelector('.time-picker .hour .hr');
+const min_element = document.querySelector('.time-picker .minute .min');
 
 // select the arrows
-const hr_up = document.querySelector(".time-picker .hour .hr-up");
-const hr_down = document.querySelector(".time-picker .hour .hr-down");
-const min_up = document.querySelector(".time-picker .minute .min-up");
-const min_down = document.querySelector(".time-picker .minute .min-down");
+const hr_up = document.querySelector('.time-picker .hour .hr-up');
+const hr_down = document.querySelector('.time-picker .hour .hr-down');
+
+const min_up = document.querySelector('.time-picker .minute .min-up');
+const min_down = document.querySelector('.time-picker .minute .min-down');
 
 // variables
-let hour = 0;
-let minute = 0;
+let d = new Date();
+let hour = d.getHours();
+let minute = d.getMinutes();
+
+// inital time
+setTime();
 
 // event listeners
 hr_up.addEventListener('click', hour_up);
 hr_down.addEventListener('click', hour_down);
 
-min_up.addEventListener('click', min_up);
-min_up.addEventListener('click', min_down);
+min_up.addEventListener('click', minute_up);
+min_down.addEventListener('click', minute_down);
+
+hr_element.addEventListener('change', hour_change);
+min_element.addEventListener('change', minute_change);
+
+// hour and minute change
+function hour_change (e) {
+  if (e.target.value > 23) {
+    e.target.value = 23;
+  } else if (e.target.value < 0) {
+    e.target.value = '00';
+  }
+
+  if (e.target.value == "") {
+    e.target.value = formatTime(hour);
+  }
+
+  hour = e.target.value;
+}
+
+function minute_change (e) {
+  if (e.target.value > 59) {
+    e.target.value = 59;
+  } else if (e.target.value < 0) {
+    e.target.value = '00';
+  }
+
+  if (e.target.value == "") {
+    e.target.value = formatTime(minute);
+  }
+
+  minute = e.target.value;
+}
 
 // helpers
 function hour_up() {
@@ -49,8 +86,8 @@ function minute_up() {
 function minute_down() {
   minute--;
   if (minute < 0) {
-    hour = 59;
-    minute--;
+    minute = 59;
+    hour--;
   }
   setTime();
 }
@@ -58,4 +95,12 @@ function minute_down() {
 function setTime() {
   hr_element.value = formatTime(hour);
   min_element.value = formatTime(minute);
+  time_picker_element.dataset.time = formatTime(hour) + ':' + formatTime(minute);
+}
+
+function formatTime (time) {
+  if (time < 10) {
+    time = '0' + time;
+  }
+  return time;
 }
